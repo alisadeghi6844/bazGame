1e6da84ee773b5d1c23d0484509fd61bbb486f4f
ali <alisadeghishiaomi@gmail.com>
2021-04-08 13:39:25 +0430

    ok-intro

--- src/components/Courser.jsx
+++ src/components/Courser.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import IntroducingGames from "./IntroducingGames";
 import RecentGames from "./RecentGames";
+import IntroductionGame from "./IntroductionGame";
 
 const Courser = () => {
     return (
         <div>
             <IntroducingGames/>
             <RecentGames/>
+            <IntroductionGame/>
         </div>
     );
 }
---
+++ src/components/IntroductionGame.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+
+const IntroductionGame = () => {
+    return (
+        <section className="introduction-game">
+            <div className="container">
+                <div className="introduction-game-box">
+                    <div className="first-intro">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <div className="box">
+                                        <div className="premium-tournament">
+                                            PREMIUM TOURNAMENT
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-lg-5 img warcraft">
+
+                                            </div>
+                                            <div className="col-lg-7 text">
+                                                <h4>World of Warcraft</h4>
+                                                <div className="ul-box">
+                                                    <ul>
+                                                        <li className="nav-item">
+                                                            <p>توسعه دهنده : <span>Blizzard Entertainment</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>ناشر : <span>Blizzard Entertainment</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>سبک : <span>نقش آفرینی</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>تاریخ انتشار : <span>November 13, 2008</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>پلتفرم: <span className="plat">PC</span></p>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-6">
+                                    <div className="box">
+                                        <div className="premium-tournament">
+                                            PREMIUM TOURNAMENT
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-lg-5 img doom">
+
+                                            </div>
+                                            <div className="col-lg-7 text">
+                                                <h4>DOOM Eternal</h4>
+                                                <div className="ul-box">
+                                                    <ul>
+                                                        <li className="nav-item">
+                                                            <p>توسعه دهنده : <span>id Software</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>ناشر : <span>Bethesda Softworks</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>سبک : <span>اکشن ماجراجویی</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>تاریخ انتشار : <span>1 فروردین 1399</span></p>
+                                                        </li>
+                                                        <li>
+                                                            <p>پلتفرم: <span
+                                                                className="plat">PC . PS4 . XBox One . NS</span></p>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default IntroductionGame;
